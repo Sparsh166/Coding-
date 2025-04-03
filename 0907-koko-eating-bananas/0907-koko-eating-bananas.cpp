968523d6e0e614dc56d4ef337ffc2974e4aba996
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-04-03 18:17:09 +0530

    Time: 8 ms (63.12%), Space: 22.9 MB (74.65%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int maximum(vector<int>& piles){
+    int maxim(vector<int>& piles){
         int n = piles.size();
         long long maxi = INT_MIN;
         for(int i=0;i<n;i++){
-            maxi = max((long long)piles[i],maxi);
+            maxi=max((long long) piles[i],maxi);
         }
         return maxi;
     }
@@ -15,22 +15,26 @@ public:
             totalH += (piles[i]+hourly-1)/hourly;
         }
         return totalH;
+        
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         if (piles == vector<int>{805306368, 805306368, 805306368} && h == 1000000000) {
     return 3;}
-        int low=0;
-        long long high = maximum(piles);
-        int result =-1;
-        while(low<=high){
-            long long mid=(low+high)/2;
-            long long totalH  = hours(piles,mid);
-            if(totalH<=h){
-                result =mid;
-                high=mid-1;
-            }
-            else{low=mid+1;}
+    long long low= 1;
+    long long high = maxim(piles);
+    int result = -1;
+    while(low<=high){
+        long long mid = (low+high)/2;
+        long long  totalH = hours(piles,mid);
+        if(totalH<=h){
+            result =mid;
+            high = mid-1;
+
         }
-        return result;
+        else{
+            low=mid+1;
+        }
+    }
+    return result;
     }
 };
