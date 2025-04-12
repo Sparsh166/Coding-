0f75b1e1d6ece8b88a2cc467f8f9bbe8a7dcd041
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-04-12 14:22:17 +0530

    Time: 98 ms (17.04%), Space: 18.7 MB (67.14%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lowerbound(vector<int>&arr, int x){
+        int n = arr.size();
+        int low =0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==x){return mid;}
+            else if (arr[mid]>x){high=mid-1;}
+            else{low=mid+1;}
+        }
+        return -1;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            int sol = lowerbound(matrix[i],target);
+            if(sol!=-1){return true;}
+        }
+        return false;
+    }
+};
