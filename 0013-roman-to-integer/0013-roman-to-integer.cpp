0e11eb33c4b602899354a2cbd001c94cbfc3960d
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-04-22 00:41:53 +0530

    Time: 16 ms (6.95%), Space: 12.1 MB (68.07%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -9,13 +9,13 @@ public:
         m.insert({'C',100});
         m.insert({'D',500});
         m.insert({'M',1000});
-        int sum = m[s[s.size() -1]];
-        for (int i=s.size()-2;i>=0;i--){
-            if(m[s[i]]<m[s[i+1]]){
-                sum-=m[s[i]];
+        int sum = m[s[s.size()-1]];
+        for(int i=s.size()-2;i>=0;i--){
+            if(m[s[i]]>=m[s[i+1]]){
+                sum+=m[s[i]];
             }
             else{
-                sum+=m[s[i]];
+                sum-=m[s[i]];
             }
         }
         return sum;
