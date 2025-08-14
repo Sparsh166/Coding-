a9fd7ea5ecbe2a7b6a1c41b95b4b962328c89f2d
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-08-14 09:55:25 +0530

    Time: 942 ms (46.43%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -5,4 +5,4 @@ cross join subjects as su
 left join examinations as e
 on s.student_id = e.student_id and su.subject_name = e.subject_name
 group by student_id, student_name, subject_name
-order by student_id, student_name 
+order by student_id, student_name
