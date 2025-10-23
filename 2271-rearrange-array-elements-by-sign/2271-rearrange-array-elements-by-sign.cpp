c2e601ab4e85176b5debe488adf0b2333d960c9b
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-10-23 23:18:38 +0530

    Time: 19 ms (5.53%), Space: 145 MB (9.06%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       vector<int> positive;
-       vector<int> negative;
-       for(int i=0;i<nums.size();i++){
-        if(nums[i]<0){
-            negative.push_back(nums[i]);
+        int n = nums.size();
+        vector<int> positive;
+        vector<int> negative;
+        for(int i =0 ;i<n;i++){
+            if(nums[i]>0){
+                positive.push_back(nums[i]);
+            }
+            else if(nums[i]<0){
+                negative.push_back(nums[i]);
+            }
         }
-        else{
-            positive.push_back(nums[i]);
-        }}
-       vector<int>result;
-       int left = 0;
-       int right = 0;
-       while (left<positive.size()&&right<negative.size()){
-        result.push_back(positive[left++]);
-        result.push_back(negative[right++]);
 
-       }
-       while (left<positive.size()){
-        result.push_back(positive[left++]);
-       }
-       while (right<negative.size()){
-        result.push_back(negative[right++]);}
+        vector<int>result;
+        int left =0;
+        int right =0;
+        while(left<positive.size() && right<negative.size()){
+         result.push_back(positive[left++]);
+         result.push_back(negative[right++]);   
+        }
+        while(left<positive.size()){
+            result.push_back(positive[left++]);
+        }
+        while(right<negative.size()){
+            result.push_back(negative[right++]); 
+        }
         return result;
     }
 };
