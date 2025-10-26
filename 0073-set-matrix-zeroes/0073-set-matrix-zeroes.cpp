0b5e008eb9cc4debec723552dda0c19acb5b6663
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-10-26 14:26:52 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -7,12 +7,13 @@ public:
         vector<int> col(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                if (matrix[i][j]==0){
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(row[i]||col[j]){
