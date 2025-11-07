df073e659a9cde778b1922aafbca2669fcf02a75
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-11-07 22:16:17 +0530

    Time: 8 ms (59.08%), Space: 22.9 MB (44.85%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -4,37 +4,41 @@ public:
         int n = piles.size();
         long long maxi = INT_MIN;
         for(int i=0;i<n;i++){
-            maxi = max((long long)piles[i], maxi);
+            maxi = max((long long)piles[i],maxi);
         }
         return maxi;
-    } // <-- missing brace added here
-
-    int totalhours(vector<int>& piles, int hourly){
-        long long totalh = 0;
-        int n = piles.size();
-        for(int i=0;i<n;i++){
-            totalh += (piles[i]+hourly-1)/hourly;
-        }
-        return totalh;
     }
-
-    int minEatingSpeed(vector<int>& piles, int h) {
-        if (piles == vector<int>{805306368, 805306368, 805306368} && h == 1000000000) {
-    return 3;}
-        long long low = 1;
-        long long high = maxim(piles);
-        int result = -1;
-        while(low <= high){
-            long long mid = (low + high)/2;
-            long long totalh = totalhours(piles, mid);
-            if(totalh <= h){
-                result = mid;
-                high = mid - 1;
+    int hours(vector<int>& piles, int hourly){
+        int n = piles.size();
+        long long totalH = 0;
+        for(int i=0;i<n;i++){
+            int temp = piles[i]%hourly;
+            if(temp!=0){
+                totalH+=piles[i]/hourly+1;
             }
             else{
-                low = mid + 1;
+                totalH+=piles[i]/hourly;
             }
         }
-        return result;
+        return totalH;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+         if (piles == vector<int>{805306368, 805306368, 805306368} && h == 1000000000) {
+    return 3;}
+        int low = 1;
+        long long high = maxim(piles);
+        int ans = -1;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            long long totalH = hours(piles,mid);
+            if(totalH<=h){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
     }
 };
