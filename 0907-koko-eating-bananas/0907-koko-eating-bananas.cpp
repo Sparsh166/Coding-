315828d58f270f89a451278470cf3bbd8fa016bb
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-11-07 19:20:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -4,37 +4,35 @@ public:
         int n = piles.size();
         long long maxi = INT_MIN;
         for(int i=0;i<n;i++){
-            maxi=max((long long) piles[i],maxi);
+            maxi = max((long long)piles[i], maxi);
         }
         return maxi;
-    }
-    int hours(vector<int>& piles,int hourly){
-        int n = piles.size();
-        long long totalH = 0;
-        for(int i=0;i<n;i++){
-            totalH += (piles[i]+hourly-1)/hourly;
-        }
-        return totalH;
-        
-    }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        if (piles == vector<int>{805306368, 805306368, 805306368} && h == 1000000000) {
-    return 3;}
-    long long low= 1;
-    long long high = maxim(piles);
-    int result = -1;
-    while(low<=high){
-        long long mid = (low+high)/2;
-        long long  totalH = hours(piles,mid);
-        if(totalH<=h){
-            result =mid;
-            high = mid-1;
+    } // <-- missing brace added here
 
+    int totalhours(vector<int>& piles, int hourly){
+        long long totalh = 0;
+        int n = piles.size();
+        for(int i=0;i<n;i++){
+            totalh += (piles[i]+hourly-1)/hourly;
         }
-        else{
-            low=mid+1;
-        }
+        return totalh;
     }
-    return result;
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long low = 1;
+        long long high = maxim(piles);
+        int result = -1;
+        while(low <= high){
+            long long mid = (low + high)/2;
+            long long totalh = totalhours(piles, mid);
+            if(totalh <= h){
+                result = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return result;
     }
 };
