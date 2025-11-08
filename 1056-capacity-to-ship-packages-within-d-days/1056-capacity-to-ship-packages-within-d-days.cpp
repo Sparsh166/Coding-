68cf5214a3d7e9830bf30798eab5975c59844675
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-11-08 17:42:01 +0530

    Time: 7 ms (90.91%), Space: 35.1 MB (17.63%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,53 +1,51 @@
 class Solution {
 public:
-    int maximum(vector<int>& weights){
+    int maxim(vector<int>& weights){
         int n = weights.size();
         int maxi = INT_MIN;
         for(int i=0;i<n;i++){
-            maxi = max(maxi,weights[i]);
+            maxi = max(weights[i],maxi);
         }
         return maxi;
     }
-
     int summation(vector<int>& weights){
         int n = weights.size();
-        int suma = 0;
+        int sum = 0;
         for(int i=0;i<n;i++){
-            suma+=weights[i];
+            sum+=weights[i];
         }
-        return suma;
+        return sum;
     }
-
-    int capacity(vector<int>& weights,int weight, int days){
+    int tdays(vector<int>& weights, int capacity){
         int n = weights.size();
-        int cnt=1;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(sum+weights[i]>weight){
-                cnt++;
-                sum=0;
+        int day = 1;
+        int load = 0;
+        for(int i=0; i<n; i++){
+            if(load+weights[i]>capacity){
+                day++;
+                load = weights[i];
             }
-            sum +=weights[i];
-            
+            else{
+                load+=weights[i];
+            }
         }
-        return cnt;
+        return day;
     }
-
     int shipWithinDays(vector<int>& weights, int days) {
-     int low = maximum(weights);
-     int high = summation(weights);
-     int ans =high;
-     while(low<=high){
-        int mid = (low+high)/2;
-        int cnt = capacity(weights,mid,days);
-        if(cnt<=days){
-            ans =mid;
-            high=mid-1;
+        int low = maxim(weights);
+        int high = summation(weights);
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int day = tdays(weights,mid);
+            if(day<=days){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        else{
-            low=mid+1;
-        }
-     }
-     return ans;   
+        return ans;
     }
 };
