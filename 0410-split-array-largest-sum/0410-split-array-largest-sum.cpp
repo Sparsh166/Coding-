b92ab0f310e81456164469852d3fdba58ac68de1
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-11-10 21:49:36 +0530

    Time: 0 ms (100%), Space: 10.5 MB (26.18%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     int maxim(vector<int>& nums){
-        int n = nums.size();
         int maxi = INT_MIN;
+        int n = nums.size();
         for(int i=0;i<n;i++){
-            maxi = max(maxi,nums[i]);
+            maxi = max(nums[i],maxi);
         }
         return maxi;
     }
-    int suma(vector<int>& nums){
+    int summ(vector<int>& nums){
+        int sum = 0;
         int n = nums.size();
-        int summ = 0;
         for(int i=0;i<n;i++){
-            summ+=nums[i];
+            sum+=nums[i];
         }
-        return summ;
+        return sum;
     }
     int arr(vector<int>& nums, int number){
+        int count = 1;
         int n = nums.size();
-        int count=1;
         int num = 0;
         for(int i=0;i<n;i++){
             if(nums[i]+num<=number){
-            num+=nums[i];
-        }
-        else{
-            count++;
-            num=nums[i];
-        }
+                num+=nums[i];
+            }
+            else{
+                count++;
+                num = nums[i];
+            }
         }
         return count;
     }
     int splitArray(vector<int>& nums, int k) {
         int low = maxim(nums);
-        int high = suma(nums);
+        int high = summ(nums);
         while(low<=high){
             int mid = (low+high)/2;
             int count = arr(nums,mid);
             if(count<=k){
-                high=mid-1;
+                high = mid-1;
             }
             else{
-                low=mid+1;
+                low = mid+1;
             }
         }
         return low;
