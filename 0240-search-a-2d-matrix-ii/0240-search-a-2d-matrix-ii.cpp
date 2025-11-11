eac18f181645a1330b45304ace5b5b66337b41d5
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-11-11 10:45:45 +0530

    Time: 96 ms (16.08%), Space: 18.8 MB (37.1%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int lowerbound(vector<int>&arr, int x){
+        int low=0;
         int n = arr.size();
-        int low =0;
         int high = n-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(arr[mid]==x){return mid;}
-            else if (arr[mid]>x){high=mid-1;}
-            else{low=mid+1;}
+            if(arr[mid]==x){
+                return mid;
+            }
+            else if(arr[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
         return -1;
     }
