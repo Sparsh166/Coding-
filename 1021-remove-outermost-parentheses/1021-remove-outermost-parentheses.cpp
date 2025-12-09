0011bbfe212ee24392b034aeba4f52d289788328
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-12-09 21:14:02 +0530

    Time: 0 ms (100%), Space: 9 MB (22.13%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.size();
+        int cnt = 0;
+        string ans = "";
+        for(int i=0;i<n;i++){
+            if(s[i]==')')cnt--;
+            if(cnt!=0){
+                ans.push_back(s[i]);
+            }
+            if(s[i]=='(')cnt++;
+        }
+        return ans;
+    }
+};
