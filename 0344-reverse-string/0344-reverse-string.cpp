b6a96b1fe908be0dda5a84de655fe37eca0e1b02
Sparsh166 <121237144+Sparsh166@users.noreply.github.com>
2025-12-21 22:02:33 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.35%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0;
+        int n = s.size();
+        int r = n-1;
+        while(l<r){
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
+
+    }
+};
